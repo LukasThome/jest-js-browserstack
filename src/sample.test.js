60066fa6db27b3b5433064591e8465862a75f14f
LukasThome <lucas.silva@ino-games.com>
2023-10-11 17:56:54 -0300

    primeiro commit

--- src/sample.test.js
+++ src/sample.test.js
@@ -1,10 +1,15 @@
-const { Builder, By, Key, until, Capabilities } = require("selenium-webdriver");
+const { Builder, By, Capabilities } = require("selenium-webdriver");
+const fs = require('fs');
 
-describe("BStack demo test", () => {
+
+
+// Defina as configurações de dispositivos que você deseja testar
+
+describe("Print Screen", () => {
   let driver;
 
-  beforeAll(() => {
-    driver = new Builder()
+  beforeAll(async () => {
+    driver = await new Builder()
       .usingServer(`http://localhost:4444/wd/hub`)
       .withCapabilities(Capabilities.chrome())
       .build();
@@ -14,59 +19,37 @@ describe("BStack demo test", () => {
     await driver.quit();
   })
   
-  test("login test", async () => {
-    await driver.get("https://bstackdemo.com");
-    await driver.wait(until.titleMatches(/StackDemo/i), 10000);
+  test("click", async () => {
+    await driver.get("https://mobile-app1-gtp106.installprogram.eu/htmlGames/3.47.1/launch/payoutTime_INOGames_3_0_4_19/mgs/payoutTime?displayName=Payout+Time!%e2%84%a2&moduleId=100999&clientId=40300&gamePath=/mgs/payoutTime&clientTypeId=40&gameId=payoutTime&languageCode=en&productId=5007&market=dotcom&brand=islandparadise&loginType=InterimUPE&returnUrl=https://mobile-app1-gtp106.installprogram.eu/lobby/en/IslandParadise/games/&routerEndPoints=&currencyFormat=&isPracticePlay=False&username=lucas&password=test1234$&formFactor=mobile");
+   
+    
 
-    await driver.wait(until.elementLocated(By.css("#signin")));
-    await driver.findElement(By.css("#signin")).click();
+    const gameCanvas = await driver.findElement(By.id("GameCanvas"));
 
-    await driver.wait(until.elementLocated(By.css("#username input")));
+    await driver.sleep(14000);
 
-    await driver
-      .findElement(By.css("#username input"))
-      .sendKeys("locked_user", Key.ENTER);
+    await gameCanvas.click();
 
-    await driver
-      .findElement(By.css("#password input"))
-      .sendKeys("testingisfun99", Key.ENTER);
+    const xPixel = 100 ; // Substitua pelo valor X em pixels
+    const yPixel = 100; // Substitua pelo valor Y em pixels
 
-    await driver.findElement(By.css("#login-btn")).click();
+    // Realize o clique nas coordenadas especificadas
+    await driver.actions().move({ x: xPixel, y: yPixel }).click().perform();
+    
+    // Esperar por 3 segundos
+    await driver.sleep(3000); 
 
-    await driver.wait(until.elementLocated(By.css(".api-error")));
+    // Tirar uma captura de tela e salvar em um arquivo
+    const screenshot = await driver.takeScreenshot();
 
-    expect(await driver.findElement(By.css(".api-error")).getText()).toBe(
-      "Your account has been locked."
-    );
-  }, 1000000);
+    // Definindo um nome para os arquivos
+    const screenshotFileName = 'teste'
 
-  test(
-    "add products to cart",
-    async () => {
-        await driver.get("https://bstackdemo.com/");
-        await driver.wait(until.titleMatches(/StackDemo/i), 10000);
+    // Definindo um local para as capturas de tela
 
-        // locating product on webpage and getting name of the product
-        await driver.wait(until.elementLocated(By.xpath('//*[@id="1"]/p')));
-        let productText = await driver
-          .findElement(By.xpath('//*[@id="1"]/p'))
-          .getText();
-        // clicking the 'Add to cart' button
-        await driver.findElement(By.xpath('//*[@id="1"]/div[4]')).click();
-        // waiting until the Cart pane has been displayed on the webpage
-        await driver.wait(until.elementLocated(By.className("float-cart__content")));
-        await driver.findElement(By.className("float-cart__content"));
-        // locating product in cart and getting name of the product in cart
-        let productCartText = await driver
-          .findElement(
-            By.xpath(
-              '//*[@id="__next"]/div/div/div[2]/div[2]/div[2]/div/div[3]/p[1]'
-            )
-          )
-          .getText();
-        // checking whether product has been added to cart by comparing product name
-        expect(productText).toBe(productCartText);
-    },
-    10000000
-  );
+
+    fs.writeFileSync(screenshotFileName, screenshot, 'base64');
+    
+    
+  });
 });
